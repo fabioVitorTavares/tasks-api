127368adff88dcc32536b21e01eaad4697746cd9
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2022-12-15 21:15:13 -0300

    commit 151220222115

--- src/server/controlers/index.ts
+++ src/server/controlers/index.ts
@@ -21,53 +21,48 @@ const taskValidations: yup.SchemaOf<TTask> = yup.object().shape({
 
 
 export const addTask = async (req: Request<TTask>, res: Response) => {
-  
-  const task = req.body;
-  console.log(task);
 
-  client.connect(async () => {
-    try {
-      const db = client.db(process.env.DB_NAME);
-      const collection = db.collection(process.env.COLLECTION_NAME as string);
-      const test = await collection.find({ date: task.date });
-      const contentTest = await test.toArray();
-      if (contentTest.length) {
-        console.log('tem');
-      }
-      else {
-        await collection.insertOne({
-          date: task.date,
-          tasks: [task]
-        });               
-      }
+  const task = {
+    id: Math.random(),
+    ...req.body,
+  };
 
-    } catch (error) {
-      console.log('Erro de conexão com o banco de dados!', error);
-    }
-  });
+  const validTask = await taskValidations.isValid(task);
+
+  if (validTask) {
   
-    //       const collection = db.collection(process.env.COLLECTION_NAME as string);
-    // const validTask = await taskValidations.isValid(req.body); 
-    // console.log(validTask);
-    
-    // if (validTask) {
-    //   client.connect(async () => {
-    //     try {
-    //       const db = client.db(process.env.DB_NAME);
-    //       const collection = db.collection(process.env.COLLECTION_NAME as string);
-    //       await collection.insertOne(doc);              
-    //       console.log('Dado inserido');
-    //     } catch (error) {
-    //       console.log('Erro de conexão com o banco de dados!', error);
-    //     }
-    //   });
-    // }
-    // else {
-    //   console.log('Invalid Task');    
-    // }
- 
-  
-    return res.send('ok post');
+   
+
+    client.connect(async () => {
+      try {
+        const db = client.db(process.env.DB_NAME);
+        const collection = db.collection(process.env.COLLECTION_NAME as string);
+        const documentDay = collection.find({ date: task.date });
+        const documentDayArray = await documentDay.toArray();
+        if (documentDayArray.length) {          
+          const documentTasksArray = await documentDayArray[0].tasks;
+          documentTasksArray.push(task);
+          await collection.updateOne({
+            date: task.date
+          }, {
+            $set: { tasks: documentTasksArray }
+          });
+        }
+        else {
+          await collection.insertOne({
+            date: task.date,
+            tasks: [task]
+          });          
+        }
+        return res.send('Task adding successfully');
+      } catch (error) {
+        console.log('Erro de conexão com o banco de dados!', error);
+      }
+    });
+  }
+  else {    
+    return res.send('Invalid Task');
+  }
 };
 
 
