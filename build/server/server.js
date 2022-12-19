09f0b7d13615e4deaa4f269be4a8c1da4b6f52a8
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2022-12-19 19:56:15 -0300

    commit 191220221956

--- build/server/server.js
+++ build/server/server.js
@@ -5,8 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.server = void 0;
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const routes_1 = require("./routes");
 const server = (0, express_1.default)();
 exports.server = server;
 server.use(express_1.default.json());
+server.use((0, cors_1.default)());
 server.use(routes_1.router);
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { router } from './routes';
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 
 server.use(router);
 
